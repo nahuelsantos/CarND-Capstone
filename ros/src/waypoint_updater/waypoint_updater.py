55e660d7cf1aa4c532ad5800045db5fe2bbc6593
unknown <xuxuandong850630@.gzps.corp.csg>
2017-09-28 12:42:46 +0800

    waypoint_updated modifed to reduce rate

--- ros/src/waypoint_updater/waypoint_updater.py
+++ ros/src/waypoint_updater/waypoint_updater.py
@@ -41,31 +41,37 @@ class WaypointUpdater(object):
         self.base_waypoints = None
         self.last_wp = None
         self.frame_id = None
+        self.loop()
+        # rospy.spin()
 
-        rospy.spin()
+    def loop(self):
+    	# published final wayponts
+
+    	rate = rospy.Rate(10)
+
+    	while not rospy.is_shutdown():
+    		rate.sleep()
+    		if self.base_waypoints is not None and self.last_pos is not None:
+				# get index closest to current position
+				self.last_wp = self.nearest_wp(self.last_pos.position, self.base_waypoints)+1
+				# fetch next LOOKAHEAD number of waypoints
+				ahead = min(len(self.base_waypoints),self.last_wp+LOOKAHEAD_WPS)
+				lookAheadWpts = self.base_waypoints[self.last_wp:ahead]
+				# construct speed for lookAheadWpts
+				for waypoint in lookAheadWpts:
+				    waypoint.twist.twist.linear.x = 4.4
+				# construct message to be sent
+				message_to_sent = Lane()
+				message_to_sent.header.stamp = rospy.Time.now()
+				message_to_sent.header.frame_id = self.frame_id
+				message_to_sent.waypoints = lookAheadWpts
+				self.final_waypoints_pub.publish(message_to_sent)
 
     def pose_cb(self, msg):
         # TODO: Implement
         self.last_pos = msg.pose
         self.frame_id = msg.header.frame_id
 
-        if self.base_waypoints is not None:
-            # get index closest to current position
-            self.last_wp = self.nearest_wp(self.last_pos.position, self.base_waypoints)+1
-            # fetch next LOOKAHEAD number of waypoints
-            ahead = min(len(self.base_waypoints),self.last_wp+LOOKAHEAD_WPS)
-            lookAheadWpts = self.base_waypoints[self.last_wp:ahead]
-            # construct speed for lookAheadWpts
-            for waypoint in lookAheadWpts:
-                waypoint.twist.twist.linear.x = 4.4
-            # construct message to be sent
-            message_to_sent = Lane()
-            message_to_sent.header.stamp = rospy.Time.now()
-            message_to_sent.header.frame_id = self.frame_id
-            message_to_sent.waypoints = lookAheadWpts
-            #rospy.logwarn("The last position is: %s", self.last_pos.position)
-            #rospy.logwarn("The last wp_index is: %d", self.last_wp)
-            self.final_waypoints_pub.publish(message_to_sent)
        
     def nearest_wp(self, last_position, waypoints):
         """find nearest waypoint index to the current location"""
