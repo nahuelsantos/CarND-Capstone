6e29bd7132be1367d7ed1fa4178635df884f7f89
Marco Ciabini <marco.ciabini@gmail.com>
2017-10-14 00:37:55 +0200

    File refactoring
    Refactored for automatic train + pb conversion script

--- ros/src/tl_detector/light_classification/graph_utils.py
+++ ros/src/tl_detector/light_classification/graph_utils.py
@@ -6,10 +6,11 @@ import keras.backend as K
 
 # Used to load the tensorflow image dimension handling in keras backend
 K.set_image_dim_ordering('tf')
+IMAGE_SIZE = 224
 
 def freeze_net(model_name):
 
-    model = SqueezeNet(3, (IMAGE_HEIGHT, IMAGE_WIDTH, 3))
+    model = SqueezeNet(3, (IMAGE_SIZE, IMAGE_SIZE, 3))
     model.load_weights("Models/{}.hdf5".format(model_name))
     # Define session and Keras configuration elements
     sess = K.get_session()
@@ -27,7 +28,7 @@ def freeze_net(model_name):
 
         output_graph_def = graph_util.convert_variables_to_constants(sess, input_graph_def, output_node_names.split(","))
         
-        with tf.gfile.GFile('Models/{}.frozen.pb'.format(model_name), "wb") as f:
+        with tf.gfile.GFile('Models/{}.pb'.format(model_name), "wb") as f:
             f.write(output_graph_def.SerializeToString())
         print("%d ops in the final graph." % len(output_graph_def.node))
 
@@ -50,6 +51,6 @@ def load_graph(graph_file, use_xla=False):
 
 
 if __name__ == '__main__':
-    freeze_net(squeezeNet_sim)
-    freeze_net(squeezeNet_real)
+    freeze_net("squeezeNet_sim_environment")
+    freeze_net("squeezeNet_sim_environment")
 
